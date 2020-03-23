1b962112fd2b809b3847e02c147c3c6a200c3ac9
Adriano Rosa <info@adrianorosa.com>
2020-03-23 18:35:06 -0300

    add react-ga

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from './components/Layout';
 import Container from '@material-ui/core/Container';
+import ReactGA from 'react-ga';
 import Covid from './Covid';
+import { app } from './config';
 
 const App = () => {
+  useEffect(() => {
+    if (app.env !== 'development') {
+      ReactGA.initialize(app.gaTrackingCode, {
+        debug: app.env === 'development',
+      });
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  });
+
   return (
     <Layout>
       <Container maxWidth="md">
--- client/src/config/app.js
+++ client/src/config/app.js
@@ -4,4 +4,5 @@ export default {
   env: process.env.NODE_ENV,
   version: process.env.REACT_APP_VERSION,
   consoleLogRequests: false,
+  gaTrackingCode: process.env.REACT_APP_GA_TRACKING_CODE
 };
